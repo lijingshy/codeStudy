2b18d5afb52f8cbba21d5ad03879613b801aec9a
lijingshy <lijingshy@gmail.com>
2017-07-27 10:31:38 +0800

    add base64

--- openssl/cipher_str.c
+++ openssl/cipher_str.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <openssl/evp.h>
 
-unsigned char* aes_128_ecb_encrypt_string(const unsigned char* in, int inl, int* outl, const unsigned char *key, bool enc)
+unsigned char* aes_128_ecb_encrypt_string(const unsigned char* in, int inl, int* outl, const unsigned char *key, bool enc, bool base64)
 {
     const EVP_CIPHER* c=EVP_aes_128_ecb();
     EVP_CIPHER_CTX ctx;
@@ -28,25 +28,70 @@ unsigned char* aes_128_ecb_encrypt_string(const unsigned char* in, int inl, int*
             return NULL;
         }
 
-        *outl = outl1+outl2;
-        return out;
+        if (base64)
+        {
+            int out1l = outl1+outl2;
+            char* out1 = strndup((char*)out, outl1+outl2);
+            memset(out, 0, EVP_ENCODE_LENGTH(inl));
+            unsigned char* out2 = (unsigned char *)OPENSSL_malloc(EVP_ENCODE_LENGTH(inl));
+            EVP_ENCODE_CTX bctx;
+            EVP_EncodeInit(&bctx);
+            EVP_EncodeUpdate(&bctx, (unsigned char*)out2, &outl1, (unsigned char*)out1, out1l);
+            memcpy(out, out2, outl1);
+            EVP_EncodeFinal(&bctx, (unsigned char*)out2, &outl2);
+            memcpy(out+outl1, out2, outl2);
+
+            OPENSSL_free(out1);
+            OPENSSL_free(out2);
+            *outl = outl1+outl2;
+            return out;
+        }
+        else
+        {
+            *outl = outl1+outl2;
+            return out;
+        }
     }
     else
     {
+        const unsigned char* din = in;
+        int dinl = inl;
+        if (base64)
+        {
+            int dbufl1, dbufl2;
+            unsigned char* dbuf = (unsigned char*)OPENSSL_malloc(inl);
+            unsigned char* tbuf = (unsigned char*)OPENSSL_malloc(inl);
+            EVP_ENCODE_CTX dctx;
+            EVP_DecodeInit(&dctx);
+            EVP_DecodeUpdate(&dctx, (unsigned char*)tbuf, &dbufl1, (unsigned char*)in, inl);
+            memcpy(dbuf, tbuf, dbufl1);
+            EVP_DecodeFinal(&dctx, (unsigned char*)tbuf, &dbufl2);
+            memcpy(dbuf+dbufl1, tbuf, dbufl2);
+
+            din = dbuf;
+            dinl = dbufl1 + dbufl2;
+
+            OPENSSL_free(tbuf);
+        }
+        
         if(!EVP_DecryptInit_ex(&ctx,c,NULL,key,NULL))
         {
+            if (base64) OPENSSL_free((unsigned char*)din);
             return NULL;
         }
 
-        if(!EVP_DecryptUpdate(&ctx,out,&outl1,in,inl))
+        if(!EVP_DecryptUpdate(&ctx,out,&outl1,din,dinl))
         {
+            if (base64) OPENSSL_free((unsigned char*)din);
             return NULL;
         }
         if(!EVP_DecryptFinal_ex(&ctx,out+outl1,&outl2))
         {
+            if (base64) OPENSSL_free((unsigned char*)din);
             return NULL;
         }
 
+        if (base64) OPENSSL_free((unsigned char*)din);
         *outl = outl1+outl2;
         return out;
     }
@@ -61,17 +106,23 @@ int main(int argc,char **argv)
     const char* key = "abc123";
     const char* in = "Do I need patent licenses to use theeee OpenSSL?";
 
+    bool base64 = true;
     int outl;
-    unsigned char* en_str = aes_128_ecb_encrypt_string((unsigned char*)in, strlen(in), &outl, (unsigned char*)key, true);
+    unsigned char* en_str = aes_128_ecb_encrypt_string((unsigned char*)in, strlen(in), &outl, (unsigned char*)key, true, base64);
 
-    for (int i = 0; i < outl; ++i)
-        printf("%02x ", en_str[i]);
-    printf("\n");
+    if (base64)
+        fwrite(en_str,1,outl,stdout);
+    else
+    {
+        for (int i = 0; i < outl; ++i)
+            printf("%02x ", en_str[i]);
+        printf("\n");
+    }
 
-    unsigned char* de_str = aes_128_ecb_encrypt_string((unsigned char*)en_str, outl, &outl, (unsigned char*)key, false);
 
-    for (int i = 0; i < outl; ++i)
-        printf("%c ", de_str[i]);
+    unsigned char* de_str = aes_128_ecb_encrypt_string((unsigned char*)en_str, outl, &outl, (unsigned char*)key, false, base64);
+
+    fwrite(de_str,1,outl,stdout);
     printf("\n");
 
     if (en_str != NULL) OPENSSL_free(en_str);
